79298f251fa0c63172cdae9ef537f71ac4b652a7
Rogerio Bento <rogeriokjrjr@hotmail.com>
2023-06-17 11:31:03 -0300

    Testing API

---
+++ src/main/java/com/apiclientes/rbjclientes/RbjClientesApplication.java
@@ -0,0 +1,13 @@
+package com.apiclientes.rbjclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbjClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbjClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiclientes/rbjclientes/RbjClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiclientes.rbjclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbjClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
